8cf4043dd3f2f4bb7089f8e27502a6eb3ad51a74
visalUpeksha <vuvisalupeksha@gmail.com>
2022-01-26 23:23:55 +0530

    Changed folder structure and changed the API Architecture to clean Architecture.

---
+++ src/movie-api-core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace movie_api_core
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/movie-api-repo/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace movie_api_repo
+{
+    public class Class1
+    {
+    }
+}
